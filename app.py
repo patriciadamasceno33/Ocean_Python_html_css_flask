8dc2ea75555ef22197cf41888c318b9dd4d93156
Patricia Damasceno <patriciandamasceno@hotmail.com>
2022-05-25 00:55:56 +0000

    Aula2 at

--- app.py
+++ app.py
@@ -12,17 +12,17 @@ def conecta_db():
 
 @app.before_request
 def antes_requisicao():
-    g.db = conecta_db()
+    g.bd = conecta_db()
 
 @app.teardown_request
 def depois_requisicao(e):
-    g.db.close()
+    g.bd.close()
 
 @app.route("/")
 def exibir_entradas():
-    sql = "SELECT titulo, texto, criado_em FROM entradaa ORDER BY id DESC;"
-    cur = g.db.execute(sql)
+    sql = "SELECT titulo, texto, criado_em FROM entradas ORDER BY id DESC;"
+    cur = g.bd.execute(sql)
     entradas = []
     for titulo, texto, criado_em in cur.fetchall():
-        entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
-    return render_template("layout.html", posts=entradas)
+        entradas.append[{"titulo": titulo, "texto": texto, "criado_em": criado_em}]
+    return render_template("layout.html", entradas=entradas)
