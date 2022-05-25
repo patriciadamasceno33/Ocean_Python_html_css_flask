91eeebbfbc1714f2b8bf6a3bd03d4c1c21f809fa
Patricia Damasceno <patriciandamasceno@hotmail.com>
2022-05-25 00:30:42 +0000

    aula2

--- app.py
+++ app.py
@@ -1,6 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, g
+import sqlite3
+
+DATABASE = "banco.bd"
+SECRET_KEY = "chave"
 
 app = Flask ("Hello")
-@app.route("/hello")
-def hello():
-    return render_template("hello.html")
+app.config.from_object(__name__)
+
+def conecta_db():
+    return sqlite3.connect(DATABASE)
+
+@app.before_request
+def antes_requisicao():
+    g.db = conecta_db()
+
+@app.teardown_request
+def depois_requisicao(e):
+    g.db.close()
+
+@app.route("/")
+def exibir_entradas():
+    sql = "SELECT titulo, texto, criado_em FROM entradaa ORDER BY id DESC;"
+    cur = g.db.execute(sql)
+    entradas = []
+    for titulo, texto, criado_em in cur.fetchall():
+        entradas.append({"titulo": titulo, "texto": texto, "criado_em": criado_em})
+    return render_template("layout.html", posts=entradas)
